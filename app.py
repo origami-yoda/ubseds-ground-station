7f8e3f3cb21b0ec1eec5e1d902ac61a82c9a868d
Nathaniel Bock <bock.nathaniel@gmail.com>
2024-03-30 14:09:41 -0400

    Packet serial radio

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask_socketio import SocketIO
 
 import json
 import time
+import serial
 from threading import Thread
-
+from radio import Parser
+from datetime import datetime
+from serial.tools.list_ports import comports
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -16,12 +19,46 @@ def replay_log():
         line_num = len(content)
         i = 0
         while True:
-            # sleep to mimic 10 hz that the fc will send packets
+            # sleep to mimic 5 hz that the fc will send packets
             time.sleep(0.2)
             msg = json.loads(content[i])
             # print(msg)
             socketio.emit("data", msg)
             i = (i + 1) % line_num
+
+def read_thread():
+    parser = Parser()
+
+    # Replace this with a yaml config thingy
+    port = "/dev/ttyUSB0"
+    s = serial.Serial(port, baudrate=115200)
+    if port is None:
+        ports = comports()
+        if len(ports) == 1:
+            port = ports[0].device
+            print("Defaulting to serial port", port)
+        else:
+            print("Unable to determine serial port to use.  Set SERIAL_PORT in config.py")
+            return
+
+    print(f"Connecting to {port}")
+    try:
+        s = serial.Serial(port, baudrate=115200)
+    except:
+        print("Could not connect to port", port)
+        print("Using 'replay.log' instead")
+        replay_log()
+
+    log_filename = datetime.now().strftime("%Y%m%dT%H%M%S.log")
+    with open(log_filename, "a", encoding="utf-8") as log_file:
+        while True:
+            buf = s.read(parser.PKT_LEN)
+            data = parser.parse(buf)
+            for msg in data:
+                print(msg)
+                socketio.emit("data", msg)
+                json.dump(msg, log_file)
+                log_file.write("\n")
 
 @app.route('/')
 def index():
@@ -30,7 +67,7 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    Thread(target=replay_log).start()
+    Thread(target=read_thread).start()
 
 @socketio.on('disconnect')
 def handle_disconnect():
