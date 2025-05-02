cf3f47b9327edafaba77cc0032ec048a17522fee
Parth Agrawal <parthnyc2005@gmail.com>
2025-05-01 23:45:11 -0400

    revamp

--- app.py
+++ app.py
@@ -17,57 +17,55 @@ port = '/dev/tty.usbserial-FT7212XY'
 serial_port = None
 
 # Connecting to serial port
-def read_thread():
-    # parser class to go frmo bytes to JSON
-    parser = Parser()
-    global port 
-    # Replace this with a yaml config thing (this is a Mistah Nate thing)
-    if port is None:
-        ports = comports()
-        if len(ports) == 1:
-            port = ports[0].device
-            print("Defaulting to serial port", port)
-        else:
-            print("Unable to determine serial port to use. Set port variable")
-            return
+# def read_thread():
+#     parser = Parser()
+#     global port 
+#     if port is None:
+#         ports = comports()
+#         if len(ports) == 1:
+#             port = ports[0].device
+#             print("Defaulting to serial port", port)
+#         else:
+#             print("Unable to determine serial port to use. Set port variable")
+#             return
 
-    print(f"Connecting to {port}")
-    try:
-        global serial_port 
-        serial_port = serial.Serial(port, baudrate=115200)
-    except Exception as e:
-        print("Could not connect to port", port)
-        print("Using 'replay.log' instead")
-        print(e)
-        # If we arent connected to a serial port, we use the replay file to test
-        replay_log()
+#     print(f"Connecting to {port}")
+#     try:
+#         global serial_port 
+#         serial_port = serial.Serial(port, baudrate=115200)
+#     except Exception as e:
+#         print("Could not connect to port", port)
+#         print("Using 'replay.log' instead")
+#         print(e)
+#         # If we arent connected to a serial port, we use the replay file to test
+#         replay_log()
 
-    log_filename = os.path.join('logs/' + datetime.now().strftime("%Y%m%dT%H%M%S.log"))
-    with open(log_filename, "a", encoding="utf-8") as log_file:
-        while True:
-            buf = serial_port.read(parser.PKT_LEN)
-            data = parser.parse(buf)
-            for msg in data:
-                print(msg)
-                socketio.emit("data", msg)
-                json.dump(msg, log_file)
-                log_file.write("\n")
+#     log_filename = os.path.join('logs/' + datetime.now().strftime("%Y%m%dT%H%M%S.log"))
+#     with open(log_filename, "a", encoding="utf-8") as log_file:
+#         while True:
+#             buf = serial_port.read(parser.PKT_LEN)
+#             data = parser.parse(buf)
+#             for msg in data:
+#                 print(msg)
+#                 socketio.emit("data", msg)
+#                 json.dump(msg, log_file)
+#                 log_file.write("\n")
 
-def replay_log():
-    # Time to wait for replay
-    time.sleep(0.2)
-    print("Using replay log...")
-    with open("logs/replay.log", "r", encoding="utf-8") as file:
-        content = file.readlines()
-        line_num = len(content)
-        i = 0
-        while True:
-            # sleep to mimic 5 hz that the fc will send packets
-            time.sleep(0.2)
-            msg = json.loads(content[i])
-            # print(msg)
-            socketio.emit("data", msg) 
-            i = (i + 1) % line_num
+# def replay_log():
+#     # Time to wait for replay
+#     time.sleep(0.2)
+#     print("Using replay log...")
+#     with open("logs/replay.log", "r", encoding="utf-8") as file:
+#         content = file.readlines()
+#         line_num = len(content)
+#         i = 0
+#         while True:
+#             # sleep to mimic 5 hz that the fc will send packets
+#             time.sleep(0.2)
+#             msg = json.loads(content[i])
+#             # print(msg)
+#             socketio.emit("data", msg) 
+#             i = (i + 1) % line_num
 
 @app.route('/')
 def index():
@@ -76,18 +74,42 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    Thread(target=read_thread).start()
+    # Thread(target=read_thread).start()
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
 
-# sending infomation back through the serial port
-@socketio.on('testingPacket')
-def testingPacket(packet):
-    print(str(packet))
-    serial_port.write(packet.encode("utf-8"))
+@socketio.on('command')
+def handle_command(command):
+    """
+    Handle switch commands from the frontend by printing to terminal.
+    Commands:
+    - 'q': N2 valve ON
+    - 'w': N2 valve OFF
+    - 'e': N2O valve ON
+    - 'r': N2O valve OFF
+    - 't': Tri-state switch UP
+    - 'y': Tri-state switch DOWN
+    """
+    command_descriptions = {
+        'q': 'N2 valve ON',
+        'w': 'N2 valve OFF',
+        'e': 'N2O valve ON',
+        'r': 'N2O valve OFF',
+        't': 'Tri-state switch UP',
+        'y': 'Tri-state switch DOWN'
+    }
     
+    description = command_descriptions.get(command, 'Unknown command')
+    print(f"COMMAND RECEIVED: '{command}' - {description}")
+    return True
 
+# sending infomation back through the serial port
+# @socketio.on('testingPacket')
+# def testingPacket(packet):
+#     print(str(packet))
+#     serial_port.write(packet.encode("utf-8"))
+    
 if __name__ == '__main__':
     socketio.run(app, debug=True)
